da1fe210c7847a9b2c343bdde4671a4814c10f8b
Michiel Dral <m.c.dral@gmail.com>
2021-09-16 14:43:32 +0000

    YOU WANTED SOMEONE WITH MORE METAPROGRAMMING EXPERIENCE HUH???

--- src/notebook.jl
+++ src/notebook.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.15.0
+# v0.16.0
 
 using Markdown
 using InteractiveUtils
@@ -53,6 +53,7 @@ md"""
 x = [1,3]
 
 # ╔═╡ 1aa24b1c-e8ca-4de7-b614-7a3f02b4833d
+# Fons wtf
 always_false(args...; kwargs...) = true
 
 # ╔═╡ 8a2e8348-49cf-4855-b5b3-cdee33e5ed67
@@ -232,10 +233,11 @@ struct CorrectCall <: Pass
 end
 
 # ╔═╡ 1bcf8bd1-c8a3-49a1-9791-d813aa856399
-# struct Error <: Fail
-# 	expr::Code
-# 	error
-# end
+Base.@kwdef struct ErrorCall <: Fail
+	expr::Code
+	steps::Vector
+	error
+end
 
 # ╔═╡ 656c4190-b49e-4225-869d-eeb7e8e41e72
 # struct Wrong <: Fail
@@ -350,7 +352,7 @@ Here, `ex2` is not a raw `Expr` — it _contains_ an evaluated array!
 
 # ╔═╡ 5b093e83-78c1-4187-b406-56e79800e1be
 md"""
-#### `Computed` struct
+### `Computed` struct
 
 Our time travel mechanism will be based on the partial evaluation principle introduced above. To differentiate between computed results and the original expression, we will wrap all computed results in a `struct`.
 """
@@ -395,58 +397,227 @@ md"""
 
 """
 
-# ╔═╡ 450a36ea-2c43-4f01-a775-0b8c59bf6dca
-function onestep_light(e::Expr; m=Module())::Vector
-	results = Any[]
-	
-	seval(ex) = Computed(Core.eval(m,ex))
-	
-	# will be modified
-	arg_results = Any[a for a in e.args]
-	
-	push_intermediate() = push!(results, Expr(e.head, arg_results...))
+# ╔═╡ 302c1fab-a358-4a5f-a3fb-4f26a17df4f6
 
-	# we only step for expressions where this is possible/easy
-	if e.head === :call || e.head === :begin || e.head === :block# || e.head === :vect
 
-		for (i,a) in enumerate(e.args)
-			
-			if a isa QuoteNode
-				a
-				
-			elseif (Meta.isexpr(e, :call) || Meta.isexpr(e, :let)) && i == 1
-				a
-				
-			elseif a isa Symbol
-				arg_results[i] = seval(a)
-				push_intermediate()
-				
-			elseif a isa Expr
-				inner_results = onestep_light(a; m=m)
-				for ir in inner_results
-					arg_results[i] = ir
-					push_intermediate()
-				end
-
-				arg_results[i] = inner_results[end]
-			else
-				a
-			end
-		end
-	end
-	
-	push!(results, seval(unwrap_computed(Expr(e.head, arg_results...))))
-	
-	results
+# ╔═╡ 7db64b02-8f64-4146-bf77-ef94cb45aae0
+function increase_counter(x, ref)
+	ref[] = ref[] + 1
+	return x
 end
 
-# ╔═╡ ef45af85-11f8-4505-9f1e-3ffb15a47142
-onestep_light(x::Any; m=Module()) = [Computed(Core.eval(m,x))]
+# ╔═╡ 83517988-08da-4031-8d51-50d79b48b73a
 
-# ╔═╡ 97a05d7c-2e08-458d-a628-7992f204b4ea
-md"""
 
-"""
+# ╔═╡ 1633fe05-cb51-4032-b6b6-f23db72bbd49
+struct FieldLens property::Symbol end
+
+# ╔═╡ 7c312943-c48b-40e7-a499-227f7ff8aa59
+struct PropertyLens property end
+
+# ╔═╡ a0207e25-0398-4104-8c0f-a8fbd9fe1d53
+struct EmptyPropertyLens end
+
+# ╔═╡ 7d14b79c-74e5-4986-80b7-de7cd7d48670
+quote2(x::Symbol) = QuoteNode(x)
+
+# ╔═╡ 5950488e-2008-48d8-9095-7f9421df191e
+quote2(x) = x
+
+# ╔═╡ 77cc33a3-c2bc-4f2d-ba88-e3693ec79b0c
+function lens_to_setter(subject, lens::Vector, value)
+	if length(lens) === 0
+		throw(ArgumentError("lens needs at least one element in path"))
+	elseif length(lens) === 1
+		property = lens[1]
+		if property isa FieldLens
+			:($subject.$(property.property) = $value)
+		elseif property isa PropertyLens
+			:($subject[$(quote2(property.property))] = $value)
+		elseif property isa EmptyPropertyLens
+			:($subject[] = $value)
+		else
+			error("Unknown lens type")
+		end
+	else
+		property, path... = lens
+		next_subject = if property isa FieldLens
+			:($subject.$(property.property))
+		elseif property isa PropertyLens
+			:($subject[$(quote2(property.property))])
+		elseif property isa EmptyPropertyLens
+			:($subject[])
+		else
+			error("Unknown lens type")
+		end
+
+		lens_to_setter(next_subject, path, value)
+	end
+end
+
+# ╔═╡ 5a3a0f63-dcce-49c9-84fd-a6317184820f
+function lens_to_getter(subject, lens::Vector)
+	if length(lens) == 0
+		return subject
+	end
+	
+	property, path... = lens
+	next_subject = if property isa FieldLens
+		:($subject.$(property.property))
+	elseif property isa PropertyLens
+		:($subject[$(quote2(property.property))])
+	elseif property isa EmptyPropertyLens
+		:($subject[])
+	else
+		error("Unknown lens type")
+	end
+
+	lens_to_getter(next_subject, path)
+end
+
+# ╔═╡ 0f31dd2e-0331-4d4c-8db5-9ce188cd3730
+lens_to_getter(nothing, [PropertyLens(:x), PropertyLens(:y)])
+
+# ╔═╡ 828e089e-64be-47c5-8e3c-7107adf02367
+lens_to_setter(Dict(:y => 1), [PropertyLens(:x)], 2)
+
+# ╔═╡ 5e66e59b-fdb8-4373-b231-097b0227dc5c
+Base.@kwdef struct CodeFrameWithExpr
+	expr
+	lens
+end
+
+# ╔═╡ f5d9a4c5-300f-4dae-8507-346ec0b74632
+function expand_lowered_to_expr(
+	lowered::Vector{CodeFrameWithExpr};
+	expr_ref
+)
+	map(lowered) do frame
+		# This whoooooole thing is to not execute parts of the code multiple times.
+		# It replaces, when possible, the arguments in the expression with a reference
+		# to the argument in the latest version of the expression
+		# (where some nodes are Computed, which we will use)
+		frame_lens = lens_to_getter(expr_ref, [
+			EmptyPropertyLens(),
+			frame.lens...,
+		])
+		expr_with_arguments_as_references = if Meta.isexpr(frame.expr, :macrocall)
+			esc(frame.expr)
+		elseif frame.expr isa Expr
+			Expr(
+				frame.expr.head,
+				map(enumerate(frame.expr.args)) do (i, arg)
+					arg_lens = :($(frame_lens).args[$(i)])
+					if Meta.isexpr(arg, :...)
+						:((
+							$(arg_lens).args[1] isa Computed ?
+							$(arg_lens).args[1].x :
+							$(esc(arg.args[1]))
+						)...)
+					elseif Meta.isexpr(arg, :call)
+						:($(arg_lens) isa Computed ? $(arg_lens).x : $(esc(arg)))
+					else
+						esc(arg)
+					end
+				end...
+			)
+		else
+			esc(frame.expr)
+		end
+
+		quote
+			# asdasdsada
+			val = $(expr_with_arguments_as_references)
+			$expr_ref[] = deepcopy($expr_ref[])
+			$(lens_to_setter(
+				expr_ref,
+				[EmptyPropertyLens(), frame.lens...],
+				:(Computed(val))
+			))
+			push!(steps, $expr_ref[])
+		end
+	end
+end
+
+# ╔═╡ af787ecf-b54e-4519-801d-eee323a1a2a3
+function lowered_with_reference_to_expr(x)
+	[CodeFrameWithExpr(expr=x, lens=[])]
+end
+
+# ╔═╡ bd1d1541-134d-4c88-bb41-920ac36bfe92
+function lowered_with_reference_to_expr(e::Expr)::Vector{CodeFrameWithExpr}
+	if e.head === :call || e.head === :begin || e.head === :block# || e.head === :vect
+		argument_frames = flatmap(enumerate(e.args)) do (i, arg)
+			
+			if arg isa QuoteNode
+				[]
+				
+			elseif (Meta.isexpr(e, :call) || Meta.isexpr(e, :let)) && i == 1
+				[]
+				
+			elseif arg isa Symbol
+				[CodeFrameWithExpr(
+						expr=arg,
+						lens=[FieldLens(:args), PropertyLens(i)],
+				)]
+				
+			elseif arg isa Expr
+				frames = lowered_with_reference_to_expr(arg)
+				map(frames) do frame
+					CodeFrameWithExpr(
+						expr=frame.expr,
+						lens=[FieldLens(:args), PropertyLens(i), frame.lens...]
+					)
+				end
+			else
+				[]
+			end
+		end
+		
+		[
+			argument_frames...,
+			CodeFrameWithExpr(
+				expr=e,
+				lens=[]
+			)
+		]
+	else
+		[
+			CodeFrameWithExpr(
+				expr=e,
+				lens=[]
+			)
+		]
+	end
+end
+
+# ╔═╡ 7dc21aa9-252f-4654-95c8-fe297f0d6021
+expand_lowered_to_expr(lowered_with_reference_to_expr(:([1:7...])); expr_ref=:expr_ref) .|> prettycolors
+
+# ╔═╡ 03579ca6-d04b-4ba7-829c-8cf3c3fdbafa
+expand_lowered_to_expr(lowered_with_reference_to_expr(:(sqrt(sqrt(1)))); expr_ref=:expr_ref) .|> prettycolors
+
+# ╔═╡ 3650d813-8a6c-4cd0-bb68-8b384e8211d8
+expand_lowered_to_expr(lowered_with_reference_to_expr(quote
+	@test 1 + 1
+end); expr_ref=:expr_ref) .|> prettycolors
+
+# ╔═╡ bed3d31a-356a-4f9e-9a58-38b511f2b0e7
+lowered_sqrt = lowered_with_reference_to_expr(:(sqrt(sqrt(4)) == 2))
+
+# ╔═╡ 907543c6-b7ed-4521-af21-d6fb86ce4518
+expand_lowered_to_expr(lowered_sqrt; expr_ref=:expr_ref) .|> prettycolors
+
+# ╔═╡ b4e5b2a0-1ccb-4e10-bd5d-a26916568d69
+xxx = lowered_with_reference_to_expr(quote
+	sqrt(sqrt(4)) == 2
+end)
+
+# ╔═╡ 0a7a0d9e-034f-4f36-a57c-34b5d4ca896d
+expand_lowered_to_expr(xxx; expr_ref=:expr_ref) .|> prettycolors
+
+# ╔═╡ ceb2c456-de9b-40a7-ac9f-3ba8d8708c2f
+lowered_with_reference_to_expr(:(4+4 ∈ [1:7...]))
 
 # ╔═╡ 88f6a040-07cf-47e0-a8be-2478ea350aa7
 can_interpret(x) = true
@@ -472,12 +643,22 @@ else
 end
 
 # ╔═╡ e1c306e3-0a47-4149-a9fb-ec7ab380fa11
-function step_by_step(expr; __module__)
+function step_by_step(expr)
 	Computed
-	onestep_light
+	expand_lowered_to_expr
+	
 	if can_interpret(expr)
+		lowered = lowered_with_reference_to_expr(expr)
+		
+		expr_ref = gensym("expr_ref")
 		quote
-			Any[$(QuoteNode(expr)), $(onestep_light)($(esc(Expr(:quote,expr))); m=$(__module__))...]
+			begin
+				expr = $(QuoteNode(expr))
+				steps = Any[expr]
+				$expr_ref = Ref{Any}(expr)
+				$(expand_lowered_to_expr(lowered; expr_ref=expr_ref)...)
+				steps
+			end
 		end
 	else
 		quote
@@ -488,8 +669,28 @@ end
 
 # ╔═╡ a661e172-6afb-42ff-bd43-bb5b787ee5ed
 macro eval_step_by_step(e)
-	step_by_step(e; __module__=__module__)
+	step_by_step(e)
 end
+
+# ╔═╡ 3fccae0c-ab69-4bc8-858b-ede886c45e32
+(@eval_step_by_step begin sqrt(sqrt(4)) + 2 end) .|> prettycolors
+
+# ╔═╡ c46d5246-e62f-4f2e-9e3a-0608c8c48b2e
+(@eval_step_by_step begin 4+4 ∈ [1:7...] end) .|> prettycolors
+
+# ╔═╡ d2e1d5ae-2daa-4cf1-8cd9-68bb8c6c81a1
+let
+	executed_count = Ref(0)
+	steps = @eval_step_by_step(begin sqrt(sqrt(increase_counter(16, executed_count))) end) .|> prettycolors
+	
+	(executed_count=executed_count, steps=steps)
+end
+
+# ╔═╡ fe02352b-962a-4ac5-ba40-6b96112235ee
+@eval_step_by_step 1 + 1
+
+# ╔═╡ ba28d507-816f-45c0-b6c0-2f5f4f09855f
+(@eval_step_by_step begin sqrt(sqrt(4)) == 2 end) .|> prettycolors
 
 # ╔═╡ 930f8244-cf25-4c1a-95f6-5c8963559c62
 @macroexpand @eval_step_by_step x == [1,2]
@@ -499,9 +700,6 @@ end
 
 # ╔═╡ 807bcd72-26c3-44d3-a295-56874cb51a89
 @eval_step_by_step xasdf = 123
-
-# ╔═╡ 8a5a4c26-e36c-4061-b32f-4448625ce4a6
-xasdf
 
 # ╔═╡ 21d4560e-721f-4ed4-9db7-86a8151ab22c
 md"""
@@ -638,21 +836,15 @@ begin
 end
 
 # ╔═╡ b6e8a170-12cc-4d97-905d-274e2609bfd8
-function test(expr, extra_args...; __module__)
+function test(expr, extra_args...)
 	step_by_step
 	Test.test_expr!("", expr, extra_args...)
 		
 	quote
-		expr_raw = $(QuoteNode(expr))
-		try
-			# steps = @eval_step_by_step($(expr))
-			
-			steps = $(step_by_step(expr; __module__=__module__))
-			
-# 			arg_results = [$((expr.args[2:end] .|> esc)...)]
-			
-# 			result = $(esc(:eval))(Expr(:call, $(expr.args[1] |> QuoteNode), arg_results...))
-			
+		local expr_raw = $(QuoteNode(expr))
+		try			
+			steps = $(step_by_step(expr))
+
 			result = unwrap_computed(last(steps))
 			
 			if result === true
@@ -793,11 +985,6 @@ ex3 = Expr(:call, :first, Computed(ex2_inner_result))
 
 # ╔═╡ 9bed78b6-5a8f-44ce-ab66-cab685daf264
 unwrap_computed(ex3)
-
-# ╔═╡ b5a9b7c1-43fb-4ec6-aaeb-0ec55580ef64
-@skip_as_script quote
-	sqrt(sqrt(4)) + 2
-end |> onestep_light .|> prettycolors
 
 # ╔═╡ 8ef356ea-7d54-43e6-a936-7c8be04c595f
 @skip_as_script onestep_light(quote
@@ -1113,10 +1300,10 @@ begin
 	export @test
 	
 	macro test(expr...)
-		test(expr...; __module__=__module__)
+		test(expr...)
 	end
 	
-	function Base.show(io::IO, m::MIME"text/html", call::Union{WrongCall,CorrectCall})
+	function Base.show(io::IO, m::MIME"text/html", call::Union{WrongCall,CorrectCall,ErrorCall})
 		
 
 		infix = if Meta.isexpr(call.expr, :call)
@@ -1243,6 +1430,9 @@ end
 # ╔═╡ f77275b9-90aa-4e07-a608-981b5df727af
 @test is_good_boy(first(friends))
 
+# ╔═╡ 37529063-8ee9-46a6-85cc-94db292da541
+@test sqrt(sqrt(16)) == sqrt(2)
+
 # ╔═╡ eab4ba31-c787-46dd-8024-693eca7fd1a0
 @test x == [1,2+2]
 
@@ -1284,11 +1474,17 @@ end
 
 # ╔═╡ 3a6a6ee1-c619-4044-b9b1-68e5ae9d2463
 map(1:10) do i
-	@test sqrt($i) < 3 && always_false()
+	@test sqrt(i) < 3 && always_false()
 end
 
 # ╔═╡ be93a6f4-b626-43db-a2fe-4e754e79c030
 @test isempty([1,sqrt(2)])
+
+# ╔═╡ e9370ce7-24ff-475a-ae47-c1de3eaeac7a
+begin
+	i = 8
+	@test sqrt(i) < 3
+end
 
 # ╔═╡ 17bd5cd9-212f-4656-ab79-590dd6c64ff8
 @test 1 ∈ [sqrt(20), 5:9...]
@@ -1300,9 +1496,16 @@ end
 @test rand(60) ∋ 1
 
 # ╔═╡ c39021dc-157c-4bcb-a3a9-fec8d9286b48
-map(1:15) do i
-	@test 2 * $i > 0.19
+map(1:15) do iii
+	try
+		@test 2 * iii > 0.19
+	catch e
+		e
+	end
 end
+
+# ╔═╡ cbab6234-5821-4a89-ab4e-e030d3494711
+@test 2 * 5 > 0.19
 
 # ╔═╡ ac02b12a-3982-4526-a51c-0bf85198b81b
 var"@test"; macroexpand(@__MODULE__, :(@test x == [1,2+i]); recursive=false) |> prettycolors
@@ -1322,11 +1525,23 @@ end
 # ╔═╡ 8150cb7e-b2e2-4ee8-a475-db4454c954f0
 embed_display(@test false)
 
+# ╔═╡ d9b35d00-64c7-472c-8890-8db5efc487b1
+@test error("Wow")
+
 # ╔═╡ 716bba60-bfbb-48a4-8924-8bf4e8958cb1
 @test always_false("asd"*"asd","asd","asd"*" asd","asd","asd"*"asd","asd","asd"*"asd","asd","asd"*"asd","asd","asd"*"asd","asd","asd"*"asd","asd")
 
 # ╔═╡ 7c1aa057-dff2-48cd-aad5-1bbc1c0a729b
 @test π ≈ 3.14 atol=0.01 rtol=1
+
+# ╔═╡ 9741e0d3-d61d-48f4-a80d-b8b24e896190
+@test [1, sqrt(sqrt(2)), 8^7]
+
+# ╔═╡ 17f0b235-bf0f-4ace-aaf1-ef3e5d325aad
+let
+	asd12 = 10
+	@test asd12 == 10
+end
 
 # ╔═╡ de62537b-a428-48d3-a866-151127b3255b
 
@@ -1337,6 +1552,7 @@ embed_display(@test false)
 # ╠═71b22e76-2b50-4d16-85f6-9dad0415630e
 # ╠═6762ed72-f422-43a9-a782-de78f739c0ae
 # ╠═f77275b9-90aa-4e07-a608-981b5df727af
+# ╠═37529063-8ee9-46a6-85cc-94db292da541
 # ╟─56347b7e-5007-45f8-8f6d-8ac8cc719637
 # ╟─bf2abe01-6ae0-4066-8704-12f64e04511b
 # ╟─e46cf3e0-aa15-4c17-a925-3e9fc5109d54
@@ -1366,6 +1582,7 @@ embed_display(@test false)
 # ╠═064e28de-0c22-48b5-b427-6eb343880287
 # ╠═3a6a6ee1-c619-4044-b9b1-68e5ae9d2463
 # ╠═be93a6f4-b626-43db-a2fe-4e754e79c030
+# ╠═e9370ce7-24ff-475a-ae47-c1de3eaeac7a
 # ╟─17bd5cd9-212f-4656-ab79-590dd6c64ff8
 # ╟─539e2c38-993b-4b3b-8aa0-f02d46d79839
 # ╟─3d3f3592-e056-4e7b-8896-a75e5b5dcad6
@@ -1385,6 +1602,7 @@ embed_display(@test false)
 # ╠═14c525a1-eca1-466b-8e63-3a90d7d7111c
 # ╟─a2efc968-246c-40c2-b285-2ec94b185a44
 # ╠═c39021dc-157c-4bcb-a3a9-fec8d9286b48
+# ╠═cbab6234-5821-4a89-ab4e-e030d3494711
 # ╠═e1c306e3-0a47-4149-a9fb-ec7ab380fa11
 # ╠═b6e8a170-12cc-4d97-905d-274e2609bfd8
 # ╟─bfe4dc61-9160-4c7e-8897-9c723b309adc
@@ -1393,11 +1611,13 @@ embed_display(@test false)
 # ╠═22a33c8c-e07f-445e-9d8d-a676f704ec45
 # ╠═176f39f1-fa36-4ce1-86ba-76248848a834
 # ╠═8150cb7e-b2e2-4ee8-a475-db4454c954f0
+# ╠═d9b35d00-64c7-472c-8890-8db5efc487b1
 # ╠═716bba60-bfbb-48a4-8924-8bf4e8958cb1
 # ╠═4bc1b7a4-0a36-4a07-b7ee-3d5be50350e1
 # ╠═3b2e8f55-1d4b-4a36-83f6-26becbd79e4b
 # ╠═7c1aa057-dff2-48cd-aad5-1bbc1c0a729b
 # ╠═ec2ed42c-1227-4e0d-b642-20e6f3503d2a
+# ╠═9741e0d3-d61d-48f4-a80d-b8b24e896190
 # ╠═9c3f6eab-b1c3-4607-add8-d6d7e468c11a
 # ╠═1ac164c8-88fc-4a87-a194-60ef616fb399
 # ╠═98ac4c36-49c7-4f65-982d-0b8bf6c372c0
@@ -1432,15 +1652,40 @@ embed_display(@test false)
 # ╠═12119016-fa61-4d38-8c58-821ea435df7d
 # ╠═9bed78b6-5a8f-44ce-ab66-cab685daf264
 # ╟─2c1b906d-71b9-430e-83ed-d4c8c0018632
-# ╠═b5a9b7c1-43fb-4ec6-aaeb-0ec55580ef64
-# ╠═450a36ea-2c43-4f01-a775-0b8c59bf6dca
-# ╠═ef45af85-11f8-4505-9f1e-3ffb15a47142
-# ╠═97a05d7c-2e08-458d-a628-7992f204b4ea
+# ╠═3fccae0c-ab69-4bc8-858b-ede886c45e32
+# ╠═c46d5246-e62f-4f2e-9e3a-0608c8c48b2e
+# ╠═7dc21aa9-252f-4654-95c8-fe297f0d6021
+# ╠═03579ca6-d04b-4ba7-829c-8cf3c3fdbafa
+# ╠═302c1fab-a358-4a5f-a3fb-4f26a17df4f6
+# ╟─7db64b02-8f64-4146-bf77-ef94cb45aae0
+# ╠═d2e1d5ae-2daa-4cf1-8cd9-68bb8c6c81a1
+# ╠═83517988-08da-4031-8d51-50d79b48b73a
+# ╠═3650d813-8a6c-4cd0-bb68-8b384e8211d8
+# ╠═f5d9a4c5-300f-4dae-8507-346ec0b74632
+# ╠═907543c6-b7ed-4521-af21-d6fb86ce4518
+# ╠═fe02352b-962a-4ac5-ba40-6b96112235ee
+# ╠═1633fe05-cb51-4032-b6b6-f23db72bbd49
+# ╠═7c312943-c48b-40e7-a499-227f7ff8aa59
+# ╠═a0207e25-0398-4104-8c0f-a8fbd9fe1d53
+# ╟─7d14b79c-74e5-4986-80b7-de7cd7d48670
+# ╟─5950488e-2008-48d8-9095-7f9421df191e
+# ╟─77cc33a3-c2bc-4f2d-ba88-e3693ec79b0c
+# ╟─5a3a0f63-dcce-49c9-84fd-a6317184820f
+# ╠═0f31dd2e-0331-4d4c-8db5-9ce188cd3730
+# ╠═828e089e-64be-47c5-8e3c-7107adf02367
+# ╠═bed3d31a-356a-4f9e-9a58-38b511f2b0e7
+# ╠═b4e5b2a0-1ccb-4e10-bd5d-a26916568d69
+# ╠═0a7a0d9e-034f-4f36-a57c-34b5d4ca896d
+# ╠═17f0b235-bf0f-4ace-aaf1-ef3e5d325aad
+# ╠═5e66e59b-fdb8-4373-b231-097b0227dc5c
+# ╠═ba28d507-816f-45c0-b6c0-2f5f4f09855f
+# ╠═ceb2c456-de9b-40a7-ac9f-3ba8d8708c2f
+# ╠═af787ecf-b54e-4519-801d-eee323a1a2a3
+# ╠═bd1d1541-134d-4c88-bb41-920ac36bfe92
 # ╠═a661e172-6afb-42ff-bd43-bb5b787ee5ed
 # ╠═930f8244-cf25-4c1a-95f6-5c8963559c62
 # ╠═68ba60db-44ad-43e4-b33e-d27696babc99
 # ╠═807bcd72-26c3-44d3-a295-56874cb51a89
-# ╠═8a5a4c26-e36c-4061-b32f-4448625ce4a6
 # ╠═8ef356ea-7d54-43e6-a936-7c8be04c595f
 # ╠═88f6a040-07cf-47e0-a8be-2478ea350aa7
 # ╠═d36a8a72-eced-4e63-9130-7fcb6c86df76
